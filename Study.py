8fb9e3868de453aa740be856a12905e510a112b6
joelDaniel <beymaxjd@gmail.com>
2025-03-20 16:26:41 -0600

    Ejercicio 1

--- Study.py
+++ Study.py
@@ -1,4 +1,17 @@
 # ==========================  
-# Funciones Matemáticas  
+# Ejercicio 1
 # ==========================  
 
+def comparar_numeros():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if num1 > num2:
+        print(f"{num1} es mayor que {num2}.")
+    elif num2 > num1:
+        print(f"{num2} es mayor que {num1}.")
+    else:
+        print("Ambos números son iguales.")
+
+# Llamada a la función
+comparar_numeros()
