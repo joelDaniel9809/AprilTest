228e9f38b35d3a88cff1ae840ce04332dac18b6c
joelDaniel <beymaxjd@gmail.com>
2025-03-20 17:03:01 -0600

    Ejercicio 2

--- Study.py
+++ Study.py
@@ -2,16 +2,47 @@
 # Ejercicio 1
 # ==========================  
 
-def comparar_numeros():
-    num1 = float(input("Ingresa el primer número: "))
-    num2 = float(input("Ingresa el segundo número: "))
+# def comparar_numeros():
+#     num1 = float(input("Ingresa el primer número: "))
+#     num2 = float(input("Ingresa el segundo número: "))
 
-    if num1 > num2:
-        print(f"{num1} es mayor que {num2}.")
-    elif num2 > num1:
-        print(f"{num2} es mayor que {num1}.")
-    else:
-        print("Ambos números son iguales.")
+#     if num1 > num2:
+#         print(f"{num1} es mayor que {num2}.")
+#     elif num2 > num1:
+#         print(f"{num2} es mayor que {num1}.")
+#     else:
+#         print("Ambos números son iguales.")
 
-# Llamada a la función
-comparar_numeros()
+# # Llamada a la función
+# comparar_numeros()
+
+# ==========================  
+# Ejercicio 2
+# ==========================  
+
+#define sum_par function
+# def sum_par():
+#     #asking for value range to the user
+#     start = int(input("Defina el inicio de los valores: "))
+#     end = int(input("Defina el final de los valores: "))
+
+#     #checking for start value not being a odd value
+#     if start % 2 != 0:
+#         #move start value to an even one
+#         start+=1
+#     #initialize Sum variable to collect even values
+#     Sum = 0
+    
+#     #going from start to end values with step 2 to check only for even values
+#     for i in range (start , end , 2):
+#         #adding even value to Sum
+#         Sum+=i
+#     #print results to user
+#     print("La suma de los valores pares es: ",Sum)
+
+# sum_par()
+
+
+# ==========================  
+# Ejercicio 3
+# ==========================  
